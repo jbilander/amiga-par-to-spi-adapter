6b270ae57e13b47d0b46d4ccbe2f4f118e2aa2d9
jbilander <jorgen.bilander@gmail.com>
2021-10-25 22:51:38 +0200

    Added script and hex file for Arduino_as_ISP

---
+++ avr/flash_Arduino_as_ISP.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Arduino\hardware\tools\avr/bin/avrdude" -C"C:\Program Files (x86)\Arduino\hardware\tools\avr/etc/avrdude.conf" -v -pm328p -carduino -PCOM4 -b115200 -Uflash:w:ArduinoISP.ino.arduino_standard.hex:i
