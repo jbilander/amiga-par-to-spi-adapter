5054efa4f97204bf31d37525db55407e0a6ce710
Jorgen Bilander <jorgen.bilander@gmail.com>
2024-11-11 19:50:48 +0100

    Added the data-lines config and a method to set them to outputs

--- esp32/main/Main.cpp
+++ esp32/main/Main.cpp
@@ -7,6 +7,8 @@
 #include <rom/ets_sys.h>
 #include <soc/gpio_reg.h>
 #include <soc/soc.h>
+#include <soc/io_mux_reg.h>
+#include <soc/gpio_sig_map.h>
 #include "Main.h"
 
 static const char *INFO_TAG = "LOG_INFO";
@@ -21,13 +23,36 @@ static void debounce_timer_callback(void *arg)
     if (card_present_n != gpio_get_level(CP_BIT_n))
     {
         card_present_n = gpio_get_level(CP_BIT_n);
-        ets_printf("LEVEL: %d\n", card_present_n);
         gpio_set_level(IRQ_BIT_n, 0);
     }
 }
 
 const esp_timer_create_args_t debounce_timer_args = {
     .callback = &debounce_timer_callback};
+
+void set_data_direction_to_output()
+{
+    PIN_INPUT_DISABLE(IO_MUX_GPIO27_REG);
+    PIN_INPUT_DISABLE(IO_MUX_GPIO26_REG);
+    PIN_INPUT_DISABLE(IO_MUX_GPIO25_REG);
+    PIN_INPUT_DISABLE(IO_MUX_GPIO23_REG);
+    PIN_INPUT_DISABLE(IO_MUX_GPIO22_REG);
+    PIN_INPUT_DISABLE(IO_MUX_GPIO21_REG);
+    PIN_INPUT_DISABLE(IO_MUX_GPIO19_REG);
+    PIN_INPUT_DISABLE(IO_MUX_GPIO18_REG);
+
+    REG_WRITE(GPIO_ENABLE_W1TS_REG, BIT(D0_BIT) | BIT(D1_BIT) | BIT(D2_BIT) | BIT(D3_BIT) |
+                                    BIT(D4_BIT) | BIT(D5_BIT) | BIT(D6_BIT) | BIT(D7_BIT));
+
+    REG_WRITE(GPIO_FUNC27_OUT_SEL_CFG_REG, 0x100);
+    REG_WRITE(GPIO_FUNC26_OUT_SEL_CFG_REG, 0x100);
+    REG_WRITE(GPIO_FUNC25_OUT_SEL_CFG_REG, 0x100);
+    REG_WRITE(GPIO_FUNC23_OUT_SEL_CFG_REG, 0x100);
+    REG_WRITE(GPIO_FUNC22_OUT_SEL_CFG_REG, 0x100);
+    REG_WRITE(GPIO_FUNC21_OUT_SEL_CFG_REG, 0x100);
+    REG_WRITE(GPIO_FUNC19_OUT_SEL_CFG_REG, 0x100);
+    REG_WRITE(GPIO_FUNC18_OUT_SEL_CFG_REG, 0x100);
+}
 
 void setup()
 {
@@ -74,6 +99,62 @@ void setup()
     io_conf.pin_bit_mask = (1ULL << IRQ_BIT_n);
     ESP_ERROR_CHECK(gpio_config(&io_conf));
 
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    io_conf.pin_bit_mask = (1ULL << D0_BIT);
+    ESP_ERROR_CHECK(gpio_config(&io_conf));
+
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    io_conf.pin_bit_mask = (1ULL << D1_BIT);
+    ESP_ERROR_CHECK(gpio_config(&io_conf));
+
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    io_conf.pin_bit_mask = (1ULL << D2_BIT);
+    ESP_ERROR_CHECK(gpio_config(&io_conf));
+
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    io_conf.pin_bit_mask = (1ULL << D3_BIT);
+    ESP_ERROR_CHECK(gpio_config(&io_conf));
+
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    io_conf.pin_bit_mask = (1ULL << D4_BIT);
+    ESP_ERROR_CHECK(gpio_config(&io_conf));
+
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    io_conf.pin_bit_mask = (1ULL << D5_BIT);
+    ESP_ERROR_CHECK(gpio_config(&io_conf));
+
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    io_conf.pin_bit_mask = (1ULL << D6_BIT);
+    ESP_ERROR_CHECK(gpio_config(&io_conf));
+
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    io_conf.pin_bit_mask = (1ULL << D7_BIT);
+    ESP_ERROR_CHECK(gpio_config(&io_conf));
+
     gpio_set_level(IRQ_BIT_n, 1);
     card_present_n = gpio_get_level(CP_BIT_n);
 
@@ -86,6 +167,10 @@ void setup()
     ESP_ERROR_CHECK(esp_timer_create(&debounce_timer_args, &debounce_timer));
 }
 
+void start_command()
+{
+}
+
 void loop()
 {
     while (true)
@@ -93,6 +178,15 @@ void loop()
         if (ISR0) // handler for REQ signal changes
         {
             ISR0 = 0;
+
+            if (REG_GET_BIT(GPIO_IN1_REG, (1 << 0))) // REQ_BIT_n
+            {
+                gpio_set_level(ACT_BIT_n, 1);
+            }
+            else
+            {
+                start_command();
+            }
         }
         if (ISR1) // handler for CP signal changes
         {
--- esp32/main/Main.h
+++ esp32/main/Main.h
@@ -1,9 +1,19 @@
 #ifndef MAIN_H_FILE
 #define MAIN_H_FILE
 
-#define IRQ_BIT_n GPIO_NUM_5 // Output, active low, open collector, external pull-up.
-#define LED_BIT GPIO_NUM_2   // Output
+// Data lines
+#define D0_BIT GPIO_NUM_27
+#define D1_BIT GPIO_NUM_26
+#define D2_BIT GPIO_NUM_25
+#define D3_BIT GPIO_NUM_23
+#define D4_BIT GPIO_NUM_22
+#define D5_BIT GPIO_NUM_21
+#define D6_BIT GPIO_NUM_19
+#define D7_BIT GPIO_NUM_18
 
+// Control lines and interrupts
+#define IRQ_BIT_n GPIO_NUM_5  // Output, active low, open collector, external pull-up.
+#define LED_BIT GPIO_NUM_2    // Output
 #define CLK_BIT GPIO_NUM_35   // Input, external pull-up.
 #define ACT_BIT_n GPIO_NUM_33 // Output, active low, internal pull-up enabled, external pull-up.
 #define CP_BIT_n GPIO_NUM_34  // Input, active low, external pull-up.
