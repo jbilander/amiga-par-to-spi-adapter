6354c97478214e098186afd50aebaafd1f09b45b
jbilander <jorgen.bilander@gmail.com>
2025-11-04 18:52:59 +0100

    Fix comments

--- rp2350/main.c
+++ rp2350/main.c
@@ -3,12 +3,12 @@
 extern void par_spi_main(void);
 extern void ftp_server_main(void);
 
-// --- Core 0 entry (FTP server) ---
+// --- Core 1 entry (FTP server) ---
 void core1_entry() {
     ftp_server_main();
 }
 
-// --- Core 1 entry (Amiga SPI bridge) ---
+// --- Core 0 entry (Amiga SPI bridge) ---
 void core0_entry() {
     par_spi_main();
 }
