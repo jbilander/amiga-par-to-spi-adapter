8a373809a5234ee1b1104cc1269b2132ab2f62cc
Jorgen Bilander <jorgen.bilander@gmail.com>
2025-11-06 12:17:51 +0100

    comment out interrupt test

--- rp2350/ftp_server.c
+++ rp2350/ftp_server.c
@@ -6,11 +6,11 @@ void ftp_server_main(void) {
     while (true) {
         // Simulate FTP activity for now
 
-        sleep_ms(10000);
-        gpio_set_dir(PIN_IRQ, true);
-        gpio_put(PIN_IRQ, 0);
-        sleep_ms(120);
-        gpio_set_dir(PIN_IRQ, false);
+        //sleep_ms(10000);
+        //gpio_set_dir(PIN_IRQ, true);
+        //gpio_put(PIN_IRQ, 0);
+        //sleep_ms(120);
+        //gpio_set_dir(PIN_IRQ, false);
 
         // Later this will be your FTP command handler:
         // ftp_handle_client();
