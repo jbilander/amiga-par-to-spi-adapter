f0b529aaf530eb031a22fbc6fec916b921fb23f9
Jorgen Bilander <jorgen.bilander@gmail.com>
2025-11-12 14:29:42 +0100

    Add Wi-Fi cyw43 running in station mode and swap cores between ftp_server_main and par_spi_main since core 1 is uninterrupted of pico_cyw43_arch_lwip_threadsafe_background that do irq and dma on core 0

--- rp2350/main.c
+++ rp2350/main.c
@@ -3,21 +3,27 @@
 #include "pico/multicore.h"
 #include "hardware/gpio.h"
 #include "hardware/spi.h"
+#include "pico/cyw43_arch.h"
+#include "lwip/netif.h"
+#include "lwip/ip4_addr.h"
 
+//FATFS fs;
 mutex_t spi_mutex;
 volatile bool amiga_wrote_to_card = false;
 
-extern void par_spi_main(void);
-extern void ftp_server_main(void);
-
-// --- Core 1 entry (FTP server) ---
-void core1_entry() {
-    ftp_server_main();
-}
+void par_spi_main(void);
+void ftp_server_main(void);
 
 // --- Core 0 entry (Amiga SPI bridge) ---
-void core0_entry() {
+void core1_entry() {
+    printf("Start Amiga SPI bridge\n");
     par_spi_main();
+}
+
+// --- Core 1 entry (FTP server) ---
+void core0_entry() {
+    printf("Start FTP Server\n");
+    ftp_server_main();
 }
 
 int main() {
@@ -26,7 +32,31 @@ int main() {
     printf("Pico USB serial active\n");
 	
     mutex_init(&spi_mutex);
+
+    // --- Wi-Fi Initialization ---
+    printf("Pico Wi-Fi test (threadsafe background)\n");
+
+    if (cyw43_arch_init()) {
+        printf("Wi-Fi init failed!\n");
+        return -1;
+    }
+
+    cyw43_arch_enable_sta_mode();
+
+    printf("Connecting to %s...\n", WIFI_SSID);
+    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+        printf("Connection failed.\n");
+        cyw43_arch_deinit();
+        return -1;
+    }
+    // --- End Wi-Fi Initialization ---
+    printf("Connected!\n");
+
+    struct netif *netif = &cyw43_state.netif[CYW43_ITF_STA];
+    printf("IP address: %s\n", ip4addr_ntoa(netif_ip4_addr(netif)));
+
     multicore_launch_core1(core1_entry);
     core0_entry();  // never returns
     return 0;
 }
+
